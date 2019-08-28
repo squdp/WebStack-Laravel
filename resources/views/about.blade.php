cf40eefa837a301e5808dd7cc3ee8951e93ac109
squdp <33194712+squdp@users.noreply.github.com>
2019-08-28 15:39:27 +0800

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,7 +13,7 @@
                 </a>
             </div>
             <div class="navbar-mobile-clear"></div>
-            <a href="https://github.com/hui-ho/WebStack-Laravel" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+       
         </div>
     </nav>
     <div class="page-container">
